5dde733d65f38512951bac7d1346ab70d17b52a7
jiangxj <1053213995@qq.com>
2020-03-04 20:06:05 +0800

    git jxj Test add user.class

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
+import po.User;
+
 public class Test {
     public static void main(String[] args) {
+        Class<User> clazz= User.class;
+        System.out.println(clazz.getName());
         System.out.println("jiangxj");
         System.out.println("second");
     }
